d74053b397d1e5cb0d26896e80244dfa7095afe3
mennan <969900135@qq.com>
2020-02-23 10:07:23 +0800

    第一次修改项目内容

--- git-test-project/src/com/itheima/gitFirstClass.java
+++ git-test-project/src/com/itheima/gitFirstClass.java
@@ -2,7 +2,10 @@ package com.itheima;
 
 public class gitFirstClass {
     public static void main(String[] args) {
+
         System.out.println("快速打印");
+        System.out.println("第一次修改");
+
     }
 
 }
